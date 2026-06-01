1f7853f722c74b5e118d69c0720536e5174024e2
Ben-jammmin <benny9952@gmail.com>
2026-06-01 15:42:52 -0700

    Initial Pygame Project Setup

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tower Defense")
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    pygame.display.update()
+
+pygame.quit()
